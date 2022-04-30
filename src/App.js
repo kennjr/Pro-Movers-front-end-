8da30e3a459c3c56fa92be016ef1a8841a9cc2f8
Eugene Crabs <eugene_crabs@Eugenes-MacBook-Air.local>
2022-04-30 08:44:34 +0300

    Created a navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Register from './components/register/Register'
 import {Routes, Route} from 'react-router-dom';
 import Login from './components/login/Login';
+import Nav from './components/nav/Nav';
 import './App.css';
 
 function App() {
   return (
     <>
-      
+      <Nav></Nav>
       <Routes>
         <Route path='login' element={<Login />}></Route>
         <Route path='register' element={<Register />}></Route>
