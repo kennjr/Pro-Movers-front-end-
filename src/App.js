a8e4645bd61341c02b4f7ba184a5266f34b557ae
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-05-02 15:52:08 +0300

    add api for posting user requests

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState,  } from 'react';
+// useEffect
 import Register from './components/register/Register'
 import {Routes, Route} from 'react-router-dom';
 import Login from './components/login/Login';
@@ -7,20 +9,59 @@ import './App.css';
 import Movers from './components/Movers';
 import Book from './components/Book';
 import Footer from './components/Footer';
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+
+ 
 function App() {
+
+const [bookings,setBookings]=useState([])
+
+  // add booking
+  const makeBooking= async (booking)=>{
+
+    const res = await fetch("http://localhost:5000/bookings",{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(booking)
+    });
+    const data = await res.json()
+
+    setBookings([...bookings,data])
+          // console.log(booking)
+          // const newBooking={...booking}
+          // setBookings([...bookings,newBooking])
+  }
+
   return (
     <>
       <Nav></Nav>
+
       <Routes>
-        <Route path="/about" element={<About />}></Route>
+        <Route path="/about" element={<About />} onBook={makeBooking}></Route>
         <Route path="movers" element={<Movers />}></Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="register" element={<Register />}></Route>
-        <Route path="movers/book" element={<Book />}></Route>
+        <Route path="movers/book" element={<Book onBook={makeBooking} />}></Route>
       </Routes>
+
+      {/* <Map google={this.props.google} zoom={14}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+         
+        </InfoWindow>
+      </Map> */}
+
       <Footer></Footer>
     </>
   );
 }
 
-export default App;
+ export default App
+
+// export default GoogleApiWrapper({
+//   apiKey: ("AIzaSyDAPcdR72o2tCqqHzPTYGJZNUiYE2dkaQA")
+// })(App);
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,43 @@
 import "../css/Booking-form.css";
+import { useState } from "react";
 
-const Book = () => {
+const Book = ({onBook}) => {
+  const [currentLocation,setCurrentLocation]=useState('');
+  const [newLocation,setNewLocation]=useState('');
+  const [mover,setMover]=useState('');
+  const [movingDate, setMovingDate] = useState("");
+  const [Package,setPackage]=useState('');
+  const [packageDescription,setPackageDescription]=useState('')
+
+  const onSubmit= (e) =>{
+    e.preventDefault()
+    if(!currentLocation || !newLocation || !mover || !Package || !movingDate || !packageDescription){
+      alert('please ensure to fill all fields')
+      return
+    }
+
+    onBook({currentLocation,newLocation,mover,movingDate,Package,packageDescription})
+        setCurrentLocation('');
+        setNewLocation('');
+        setMover('');
+        setMovingDate('')
+        setPackage('');
+        setPackageDescription('')
+  }
   return (
     <>
       <div className="book">
-        <form action="" className="book-form">
+        <form action="" className="book-form" onSubmit={onSubmit}>
           <div className="book-form-title">
-            <p>Book Your mover <br /></p> 
+            <p>
+              Book Your mover <br />
+            </p>
           </div>
           <div className="currentLocation-input">
             <input
               type="text"
+              value={currentLocation}
+              onChange={(e) => setCurrentLocation(e.target.value)}
               name="currentLocation"
               id="currentLocation"
               placeholder="currentLocation"
@@ -19,37 +46,59 @@ const Book = () => {
           <div className=" newLocation">
             <input
               type="text"
+              value={newLocation}
+              onChange={(e) => setNewLocation(e.target.value)}
               name="newLocation"
               id="newLocation"
               placeholder="New location"
             />
           </div>
           <div className="mover">
-            <input type="text" name="mover" id="mover" placeholder="mover" />
+            <input
+              type="text"
+              name="mover"
+              id="mover"
+              placeholder="mover"
+              value={mover}
+              onChange={(e) => setMover(e.target.value)}
+            />
           </div>
           <div className="movingDate">
             <input
               type="date"
+              value={movingDate}
+              onChange={(e) => setMovingDate(e.target.value)}
               name="movingDate"
               id="movingDate"
               placeholder="Your moving date"
             />
           </div>
-          <div className=" package">
+          <div className=" Package">
             <input
               type="text"
-              name="package"
-              id="package"
+              value={Package}
+              onChange={(e) => setPackage(e.target.value)}
+              name="Package"
+              id="Package"
               placeholder="What we are moving for you"
             />
           </div>
           <div className="packageDescription">
-            
-            <textarea name="packageDescription" id="packageDescription" cols="30" rows="8" placeholder="Describe your package"></textarea>
+            <textarea
+              name="packageDescription"
+              id="packageDescription"
+              cols="30"
+              rows="8"
+              placeholder="Describe your package"
+              value={packageDescription}
+              onChange={(e) => setPackageDescription(e.target.value)}
+            ></textarea>
           </div>
 
           <div className="Book">
-            <button type="submit">Book</button>
+            <button type="submit" value="book">
+              Book
+            </button>
           </div>
         </form>
       </div>
